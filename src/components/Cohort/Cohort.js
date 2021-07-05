9259a252a329e34e5ea8db3136c5e8fbe96ef970
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-07-05 17:42:21 -0600

    Add code comments to future components

--- src/components/Cohort/Cohort.js
+++ src/components/Cohort/Cohort.js
@@ -3,5 +3,12 @@ import Person from '../Person/Person';
 import './Cohort.css';
 
 // COHORT COMPONENT CODE GOES HERE
+//acts as a container for Person
+//has state
+//passes props to Person cards 
+//creates and presents person cards 
+
+//use .map to render the person cards 
+//is a child of app?
 
 export default Cohort;
--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -3,4 +3,9 @@ import './Person.css';
 
 // PERSON COMPONENT CODE GOES HERE
 
+//is a functional component
+//accepts props 
+//renders a card with props
+//will render: image, name, quote, superlative
+
 export default Person;
